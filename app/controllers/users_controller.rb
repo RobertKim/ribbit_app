a678de0fe2df331a0d76e6b0cc210993c4780d41
Robert Kim <robertjoonkim@gmail.com>
2013-12-19 00:11:10 -0800

    created usersController#new, #create methods; users/new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user]) #Failed validation will render 'new' auto-populated.
+		if @user.save
+			redirect_to @user, notice: "Thank you for signing up to Ribbit!"
+		else
+			render 'new'		
+		end
+		
+	end
 end
