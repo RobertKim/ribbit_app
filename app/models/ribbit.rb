dd5e931a58e60bdde421bc5cd3452ac32eba2332
Robert Kim <robertjoonkim@gmail.com>
2013-12-19 03:01:29 -0800

    associated ribbit and user classes

--- app/models/ribbit.rb
+++ app/models/ribbit.rb
@@ -3,6 +3,7 @@ class Ribbit < ActiveRecord::Base
 	attr_accessible :content, :user_id
 	default_scope order: "created_at DESC"
 
+	belongs_to :user
 
 	validates :content, length: { maximum: 140 }
 end
