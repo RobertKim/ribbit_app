36804c4868c9abd76341c395b8def8276b55a99b
Robert Kim <robertjoonkim@gmail.com>
2013-12-30 02:22:49 -0800

    added profiles show and current user profile show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,12 @@ class UsersController < ApplicationController
 		
 	end
 
+	def index
+		@users = User.all
+		@ribbit = Ribbit.new
+	end
+
+
 	def show
 		@user = User.find(params[:id])
 		@ribbit = Ribbit.new
