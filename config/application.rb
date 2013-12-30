1861dc917cc74b80fe0b566ab47052a4b3713bf2
Robert Kim <robertjoonkim@gmail.com>
2013-12-30 04:46:49 -0800

    configured which assets to precompile again

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module RibbitApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = false
+
+    config.assets.precompile = ["ribbits.js"]
     
   end
 end
